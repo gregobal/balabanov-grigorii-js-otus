567d571a888fcb2e4fdf037e3ce3d696002b1bc0
Grigorii Balabanov <810411@mail.ru>
2018-11-13 15:51:13 +0500

    homework 3, 0.1.2

--- node/node-1/request.js
+++ node/node-1/request.js
@@ -2,12 +2,12 @@ const request = require('request');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-let reqCount = parseInt(process.argv[2], radix = 10);
-let reqType = process.argv[3];
+const reqCount = parseInt(process.argv[2], 10);
+const reqType = process.argv[3];
 
 function get(url) {
   return new Promise(resolve => {
-    let startTime = Date.now();
+    const startTime = Date.now();
     request(url, (error, response, body) => {
       if (error) {
         throw error;
@@ -17,40 +17,36 @@ function get(url) {
   });
 }
 
-if (isNaN(reqCount) || reqCount < 1) {
-  throw ('Первым аргументом необходимо передать количество запросов, число >= "1"');
-}
-
-if (reqType === 'a') {
-  const req = [];
-
-  for (let i = 0; i < reqCount; i++) {
-    req.push(get(`http://${hostname}:${port}/${i + 1}`))
-  }
-
-  Promise.all(req)
-    .then(results => {
-      results.forEach(result => console.log(result))
-    })
-    .catch(error => {
-      console.error(error)
-    })
-} else if (reqType === 'c') {
+function fillUrlsArr() {
   const urls = [];
 
   for (let i = 0; i < reqCount; i++) {
     urls.push(`http://${hostname}:${port}/${i + 1}`)
   }
 
-  urls.reduce((memo, url) => {
+  return urls;
+}
+
+if (isNaN(reqCount) || reqCount < 1) {
+  throw ('Первым аргументом необходимо передать количество запросов, число >= "1"');
+}
+
+if (reqType === 'a') {
+  Promise.all(fillUrlsArr().map(url => get(url)))
+    .then(results => {
+      results.forEach(result => console.log(result))
+    })
+    .catch(console.error)
+
+} else if (reqType === 'c') {
+  fillUrlsArr().reduce((memo, url) => {
     return memo
       .then(() => get(url)
-        .then(result => console.log(result))
-        .catch(error => {
-          console.error(error)
-        })
+        .then(console.log)
+        .catch(console.error)
       )
   }, Promise.resolve())
+
 } else {
   throw ('Вторым аргументом необходимо передать тип запросов: "a" - параллельный или "c" - последовательный');
 }
